ef5b82481eb01dd4e8080d024060b3638618ec85
ATRIX GAMING <theokazmaros@gmail.com>
2023-07-02 09:37:34 +1000

    polished python venv script

--- src/run.sh
+++ src/run.sh
@@ -4,11 +4,12 @@
 
 echo "Starting Python Virtual Environment..."
 
-python -m venv venv
+python -m venv env
+source env/Scripts/activate
 
 ./dependency_check.sh
 
 python3 ./main.py
 
 deactivate
-# rm -r venv
+rm -r env
