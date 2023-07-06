dc37374f6dcd9437d96f94a45a1e0b6f6a794938
ATRIX GAMING <theokazmaros@gmail.com>
2023-07-06 19:15:22 +1000

    updated run shell script, added venv activation check

--- src/run.sh
+++ src/run.sh
@@ -6,10 +6,11 @@ echo "Starting Python Virtual Environment..."
 
 python3 -m venv env
 
+if [ -f "env/bin/activate" ]; then
+  source env/bin/activate
+fi
 if [ -f "env/Scripts/activate" ]; then
   source env/Scripts/activate
-else
-  source env/bin/activate
 fi
 
 pip install -r requirements.txt
