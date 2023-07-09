ac58fef6e376e273e89c29a16deff49694340587
ATRIX GAMING <theokazmaros@gmail.com>
2023-07-09 12:55:40 +1000

    added clear terminal at application start

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,10 +1,11 @@
 from user_input import UserInput
 from interface import Interface
 from logic import Logic
+from terminal import Terminal
 
 
 if __name__ == "__main__":
-
+    Terminal.clear_terminal()
     Interface.start_interface()
     UserInput.enter_to_continue()
     Logic.run_logic()
